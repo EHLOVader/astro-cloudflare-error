e6a53a3a5313464f109d21f93dc5767b39b06e40
ehlovader <j4h.r8n+github@gmail.com>
2023-01-11 16:04:43 -0500

    :recycle: :wrench: Update the build directory to be public for gitlab and adjust config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,23 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
+// // if in production we need a base path for the gitlab pages
+// const base = process.env.NODE_ENV === 'production' ? '/astro-cloudflare-error' : '/';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+
+  // GitLab Pages requires exposed files to be located in a folder called "static".
+  // So we're instructing Astro to put the static build output in a folder of that name.
+  outDir: 'public',
+
+  // The folder name Astro uses for static files (`static`) is already reserved
+  // for the build output. So in deviation from the defaults we're using a folder
+  // called `static` instead.
+  publicDir: 'static',
+
+  // // GitLab pages are served from a directory not the root of the domain.
+  // base: base
+
 });
